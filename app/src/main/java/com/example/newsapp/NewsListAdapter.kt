9b5abb1e59aabe325c8dd1a4fc3a3dd74fc25f4e
Saransh <saransh0701@gmail.com>
2021-04-17 19:42:13 +0530

    Add author and image

--- app/src/main/java/com/example/newsapp/NewsListAdapter.kt
+++ app/src/main/java/com/example/newsapp/NewsListAdapter.kt
@@ -3,8 +3,10 @@ package com.example.newsapp
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import com.bumptech.glide.Glide
 
 class NewsListAdapter(private val listener: NewsItemClicked) : RecyclerView.Adapter<NewsViewHolder>() {
 
@@ -22,6 +24,8 @@ class NewsListAdapter(private val listener: NewsItemClicked) : RecyclerView.Adap
     override fun onBindViewHolder(holder: NewsViewHolder, position: Int) {
         val current = item[position]
         holder.titleView.text = current.title
+        holder.author.text = current.author
+        Glide.with(holder.itemView.context).load(current.imageUrl).into(holder.image)
     }
 
     override fun getItemCount(): Int {
@@ -38,6 +42,8 @@ class NewsListAdapter(private val listener: NewsItemClicked) : RecyclerView.Adap
 
 class NewsViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
     val titleView : TextView = itemView.findViewById(R.id.title)
+    val image: ImageView = itemView.findViewById(R.id.image)
+    val author: TextView = itemView.findViewById(R.id.author)
 }
 
 interface NewsItemClicked {
